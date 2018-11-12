e29253b6b51ef824f2137fa724f69a667d8f3369
github username <schun08@gmail.com>
2018-11-12 17:39:53 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,56 @@
+// first attempt 5/13/18
+// import React from 'react';
+// import TweetWall from './TweetWall';
+//
+// import { getTweets }from '../lib/mockAPI';
+//
+// class App extends React.Component {
+//
+//   constructor() {
+//     super();
+//
+//     this.state = {
+//       latestTweets: []
+//     };
+//   }
+//
+//   // TODO: componentWillMount()
+//   componentWillMount() {
+//     this.fetchTweets();
+//   }
+//   // TODO: componentDidMount()
+//   componentDidMount(){
+//     this.startInterval();
+//   }
+//   // TODO: componentWillUnmount()
+//   componentWillUnmount(){
+//     this.cleanUpInterval();
+//   }
+//
+//   startInterval = () => {
+//     this.interval = setInterval(this.fetchTweets, 2000);
+//   }
+//
+//   cleanUpInterval = () => clearInterval(this.interval);
+//
+//   fetchTweets = () => {
+//     const newTweets = getTweets();
+//     this.setState({
+//       latestTweets: newTweets
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <TweetWall newTweets={this.state.latestTweets} />
+//       </div>
+//     )
+//   }
+// }
+//
+// export default App;
+// second attempt 11/12/18
 import React from 'react';
 import TweetWall from './TweetWall';
 
@@ -15,17 +68,17 @@ class App extends React.Component {
 
   // TODO: componentWillMount()
   componentWillMount() {
-    this.fetchTweets();
+    this.fetchTweets()
   }
   // TODO: componentDidMount()
-  componentDidMount(){
-    this.startInterval();
+  componentDidMount() {
+    this.startInterval()
   }
   // TODO: componentWillUnmount()
-  componentWillUnmount(){
-    this.cleanUpInterval();
+  componentWillUnmount() {
+    this.cleanUpInterval()
   }
-
+  
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
   }
--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -1,3 +1,45 @@
+// first attempt 5/13/18
+// import React from 'react';
+// import Tweet from './Tweet';
+//
+// class TweetWall extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       tweets: []
+//     };
+//   }
+//
+//   // TODO: componentWillMount()
+//   componentWillMount(){
+//     this.setState({
+//       tweets: this.props.newTweets
+//     });
+//   }
+//   // TODO: shouldComponentUpdate()
+//   shouldComponentUpdate(nextProps, nextState){
+//     return (nextProps.newTweets.length > 0);
+//   }
+//   // TODO: componentWillReceiveProps()
+//   componentWillReceiveProps(nextProps){
+//     this.setState({
+//       tweets: [...nextProps.newTweets, ...this.state.tweets]
+//     })
+//   }
+//
+//   render() {
+//     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
+//
+//     return (
+//       <div>{tweets}</div>
+//     );
+//   }
+// }
+//
+// export default TweetWall;
+// second attempt 11/12/18
+
 import React from 'react';
 import Tweet from './Tweet';
 
@@ -11,19 +53,20 @@ class TweetWall extends React.Component {
   }
 
   // TODO: componentWillMount()
-  componentWillMount(){
+  componentWillMount() {
     this.setState({
       tweets: this.props.newTweets
-    });
+    })
   }
   // TODO: shouldComponentUpdate()
-  shouldComponentUpdate(nextProps, nextState){
-    return (nextProps.newTweets.length > 0);
+  shouldComponentUpdate(nextProps){
+    return (nextProps.length > 0)
   }
+
   // TODO: componentWillReceiveProps()
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     this.setState({
-      tweets: [...nextProps.newTweets, ...this.state.tweets]
+      tweets: nextProps.newTweets.concat(this.state.tweets)
     })
   }
 
